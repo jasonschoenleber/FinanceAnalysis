6871000bdb911caee4bc86db20c382ccef2758e7
jasonschoenleber <jasonschoenleber@github.com>
2021-02-01 20:37:48 -0600

    Added Files

---
+++  (1).ipynb_checkpoints/stocktrade-checkpoint.py
@@ -0,0 +1,98 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Imports needed\n",
+    "import pandas as pd\n",
+    "%matplotlib inline"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Get the data\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Calculate the inputs that go into the model like the 200sma and 50sma (sma = simple moving average)\n",
+    "\n",
+    "df['% change'] = df['Close'].pct_change()\n",
+    "df['200 sma'] = df['Close'].rolling(window=200).mean().round(5)\n",
+    "df['50 sma'] = df['Close'].rolling(window=50).mean().round(5)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Model Criteria\n",
+    "\n",
+    "df['Criteria 2'] = (df['50 sma'] >= df['200 sma']) | df['Criteria 1'] == True\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Calculate the Model\n",
+    "\n",
+    "df['Model'] = 100*(1+df['Criteria 2'].shift(1)*df['% change']).cumprod()\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Calculate the Models' returns\n",
+    "\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
