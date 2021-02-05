405b2a3f617acb485f1e2b28441597dc8a166672
jasonschoenleber <jasonschoenleber@github.com>
2021-02-05 13:42:00 -0600

    Initial Commit

---
+++ Trading Bot/tradebot.py
@@ -0,0 +1,5 @@
+#This is my first attempt at building a trading bot.  Trading Strategy below:
+#Breakout strategy - the bot will purchase a stock when it identifies it is on the rise
+#Stop-loss strategy - the bot will sell a stock when it identifies it is falling
+#Both strategies will utilize a volatility adjusted lockback length approach
+
